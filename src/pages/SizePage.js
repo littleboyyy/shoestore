9d74a8769f826233dbf6b5b6b6a8f1cc790b34e4
leduochoang2001 <hoangithub147@gmail.com>
2023-01-27 19:46:16 +0700

    size-page

--- src/pages/SizePage.js
+++ src/pages/SizePage.js
@@ -6,6 +6,12 @@ function SizePage() {
         <div style={{ textAlign: 'center' }}>
             <h1>Shoe Size Chart</h1>
             <img src={size_chart} alt="size chart" />
+            <br /><br /><br />
+            <div style={{ color: 'red', fontSize: '20px' }}>
+                <strong>We use EU size for products</strong>
+                <br />
+                <strong>Please change your size to EU size to get the suitable products!</strong>
+            </div>
         </div>
 
     )
