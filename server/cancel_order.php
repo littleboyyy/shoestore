515c65cef7e7516b92a5a959b202464802bdce94
Tran Van Phu <82558208+tranphu033@users.noreply.github.com>
2023-02-08 10:46:07 +0700

    new_update

---
+++ server/cancel_order.php
@@ -0,0 +1,19 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+require_once("connect_db.php");
+
+$query="SELECT MAX(orderID) from shoe_order";
+$res = mysqli_query($conn, $query);
+if (!$res) die("Failed to excute SQL query: $query<br>");
+$row=mysqli_fetch_row($res);
+$max_orderID=$row[0];
+
+//xoa don hang moi them:
+$query = "DELETE FROM shoe_order WHERE orderID=$max_orderID";
+$res = mysqli_query($conn, $query);
+if (!$res) die("Failed to excute SQL query: $query<br>");
+
+mysqli_free_result($res);
+mysqli_close($conn);
--- server/delete_prod.php
+++ server/delete_prod.php
@@ -4,10 +4,29 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 //xoa theo shoeID (ma giay)
 require_once("connect_db.php");
+
 $shoeID = $_POST['shoeID'];
 
-$query = "DELETE FROM shoe WHERE shoeID=$shoeID";
+$query="SELECT MAX(shoeID) from shoe";
+$res = mysqli_query($conn, $query);
+if (!$res) die("Failed to excute SQL query: $query<br>");
+$row=mysqli_fetch_row($res);
+$max_shoeID=$row[0];
 
+$query = "DELETE FROM shoe WHERE shoeID=$shoeID";
+$res = mysqli_query($conn, $query);
+if (!$res) die("Failed to excute SQL query: $query<br>");
+
+//update lai ID cua shoes
+$k=$shoeID;
+for($i=$shoeID+1;$i<=$max_shoeID;$i++){
+    $query = "UPDATE shoe SET shoeID=$k WHERE shoeID=$k+1";
+    $res = mysqli_query($conn, $query);
+    if (!$res) die("Failed to excute SQL query: $query<br>");
+    $k++;
+}
+
+$query = "ALTER TABLE shoe AUTO_INCREMENT=$max_shoeID";
 $res = mysqli_query($conn, $query);
 if (!$res) die("Failed to excute SQL query: $query<br>");
 
